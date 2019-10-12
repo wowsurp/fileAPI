1078edb5ac3d502484d55a5c29416ea92659cb1a
wowsurp <dkswhdgk@gmail.com>
2019-10-12 16:34:34 +0900

    mysql jpa 설정

--- src/main/java/com/example/fileapi/entity/File.java
+++ src/main/java/com/example/fileapi/entity/File.java
@@ -1,10 +1,29 @@
 package com.example.fileapi.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "tbfile")
 public class File {
 
-	private String fileNm;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idx;
+	
+	@Column(length = 50)
+	private String name;
+	
+	private long size;
+	
+	@Column(length = 5)
+	private String extension;
 	
 }
